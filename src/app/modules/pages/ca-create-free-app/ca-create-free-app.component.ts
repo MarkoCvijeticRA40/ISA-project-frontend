2f7e7c762d6aaf34befabc833572443596b8978d
MarkoCvijeticRA40 <93485029+MarkoCvijeticRA40@users.noreply.github.com>
2022-12-21 13:52:41 +0100

    Fix problems

--- src/app/modules/pages/ca-create-free-app/ca-create-free-app.component.ts
+++ src/app/modules/pages/ca-create-free-app/ca-create-free-app.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Center } from 'src/app/model/center.model';
 import { Freeapointment } from 'src/app/model/freeapointment.model';
 import { Medicalstaff } from 'src/app/model/medicalstaff.model';
-import { RegisteredUser } from 'src/app/model/registered-user.model';
 import { FreeappointmentService } from 'src/app/service/freeappointment.service';
 import { UserService } from 'src/app/service/user.service';
 import { Address } from 'src/app/model/address.model';
+import { CenterService } from 'src/app/service/center.service';
 
 @Component({
   selector: 'app-ca-create-free-app',
@@ -28,7 +28,6 @@ export class CaCreateFreeAppComponent implements OnInit {
 
   minute : number = 0;
   
-  
   hours = [
     {value: 8,  viewValue: '08'},
     {value: 9,  viewValue: '09'},
@@ -36,6 +35,15 @@ export class CaCreateFreeAppComponent implements OnInit {
     {value: 11, viewValue: '11'},
     {value: 12, viewValue: '12'},
     {value: 13, viewValue: '13'},
+    {value: 14, viewValue: '14'},
+    {value: 15, viewValue: '15'},
+    {value: 16, viewValue: '16'},
+    {value: 17, viewValue: '17'},
+    {value: 18, viewValue: '18'},
+    {value: 19, viewValue: '19'},
+    {value: 20, viewValue: '20'},
+    {value: 21, viewValue: '21'},
+    {value: 22, viewValue: '22'},
   ];
 
   minutes = [
@@ -52,7 +60,7 @@ export class CaCreateFreeAppComponent implements OnInit {
     {value: 60, viewValue: "60"},
   ];
   
-  constructor(private freeAppointmentService: FreeappointmentService,private userService: UserService) 
+  constructor(private freeAppointmentService: FreeappointmentService,private userService: UserService,private centerService : CenterService) 
   { 
     this.startDate.setDate(this.startDate.getDate() + 1);
   }
@@ -78,13 +86,14 @@ export class CaCreateFreeAppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.centerForFreeApp.id = 1;
-    this.freeAppointment.duration = 15;
-    this.centerForFreeApp.address = this.adressForFreeApp;
-    this.freeAppointment.center = this.centerForFreeApp;
     this.userService.findAdministrator("milan@gmail.com").subscribe(res => {
       this.medicalStaff = res;  
         this.freeAppointment.medicalStaff = this.medicalStaff; 
     })
+    this.centerService.getCenterByMedicalStaffId(1).subscribe(res => {
+      this.centerForFreeApp = res;   
+        this.freeAppointment.center = this.centerForFreeApp;
+    })
+    this.freeAppointment.duration = 15;
   }
 }
